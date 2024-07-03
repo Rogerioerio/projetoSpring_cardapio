2af3dff2fa506138a2c565b48a5992e02c0ec67c
Rogério GM <124842207+Rogerioerio@users.noreply.github.com>
2024-07-03 20:26:54 -0300

    Preparando Ambiente com Repositorio e Entidade

--- cardapio/src/main/java/com/projeto/cardapio/controller/FoodController.java
+++ cardapio/src/main/java/com/projeto/cardapio/controller/FoodController.java
@@ -1,5 +1,6 @@
 package com.projeto.cardapio.controller;
 
+import com.projeto.cardapio.controller.food.Food;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class FoodController {
     @GetMapping
     public void getAll() {
 
+        Food food;
     }
 
 
---
+++ cardapio/src/main/java/com/projeto/cardapio/controller/food/FoodRepository.java
@@ -0,0 +1,6 @@
+package com.projeto.cardapio.controller.food;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
